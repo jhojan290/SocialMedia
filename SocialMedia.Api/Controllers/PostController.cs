ce946fcf8a7e7eb88b1fc49f3be330c6adf2812c
jhojan290 <davio13bar@gmail.com>
2024-02-14 18:45:24 -0500

    nuevo-commit

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infraestructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPost()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok(posts);
+        }
+    }
+}
---
+++ SocialMedia.Infraestructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infraestructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            });
+
+            return posts;
+        }
+    }
+}
